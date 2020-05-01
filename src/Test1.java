8d53cd5e1b42a1a0ed8dac1f3536d0ee00ce0ecd
Thanh <thanhphamnl@gmail.com>
2020-04-30 21:31:19 -0700

    Update funchtion static void main

--- src/Test1.java
+++ src/Test1.java
@@ -12,8 +12,12 @@ public class Test1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is the first git program");
+		String hungry  = "yes";
+		if(hungry == "yes") {
+			System.out.println("Eat something ....");
+		}else {
+			System.out.println("Do work");
+		}		
 	}
 
 }
